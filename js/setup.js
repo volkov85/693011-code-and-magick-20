a9e093ddcf6cdda4a66aad1d9bab23cabe0f4ec9
Volkov85 <xplz2007@gmail.com>
2020-06-14 15:25:43 +0600

    Рефакторинг кода

--- js/setup.js
+++ js/setup.js
@@ -1,65 +1,142 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
+var WIZARD = {
+  COUNT: 4,
+  NAMES: [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ],
+  SURNAMES: [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ],
+  COATS: [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ],
+  EYES: [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ]
+};
 
-userDialog.classList.remove('hidden');
-
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-
-// Случайный порядок имени и фамилии по алгоритму Фишера — Йетса
-var shuffle = function (array) {
+/**
+ * Перемешивание массива по алгоритму Фишера — Йетса
+ * @param  {Array} array - исходный массив
+ * @return {Array} array - перемешанный массив
+ */
+var shuffleArray = function (array) {
   for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
-    var t = array[i]; array[i] = array[j]; array[j] = t; // поменять элементы местами
+    var j = Math.floor(Math.random() * (i + 1));
+    var t = array[i];
+    array[i] = array[j];
+    array[j] = t;
   }
   return array;
 };
 
-// Создание массива со случайными именем и фамилией и преобразование в строку
-var nameGenerate = function () {
-  var randomName = WIZARD_NAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))];
-  var randomSurename = WIZARD_SURNAMES[Math.floor(Math.random() * (WIZARD_SURNAMES.length))];
-  var nameArray = [randomName, randomSurename];
-
-  return shuffle(nameArray).join(' ');
+/**
+ * Генерирует рандомное число в диапазоне (Максимум и минимум включаются)
+ * @param  {number} min - от какого числа
+ * @param  {number} max - до какого числа
+ * @return {number} - рандомное число из диапазона
+ */
+var getRandomNumber = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// Заполнение массива wizards объектами
-var wizards = [];
-for (var i = 0; i < 4; i++) {
-  wizards.push(
-      {
-        name: nameGenerate(),
-        coatColor: WIZARD_COATS[Math.floor(Math.random() * (WIZARD_COATS.length))],
-        eyesColor: WIZARD_EYES[Math.floor(Math.random() * (WIZARD_EYES.length))]
-      }
-  );
-}
 
-// Создание DOM элемента на основе JS объекта
+/**
+ * Берет случайный элемент из массива
+ * @param  {Array} array - исходный массив
+ * @return {string} - рандомный элемент из массива
+ */
+var getRandomElementFromArray = function (array) {
+  return array[getRandomNumber(0, array.length - 1)];
+};
+
+/**
+ * Создание массива со случайными именем и фамилией, перемешивание и преобразование в строку
+ * @return {string} - строка вида 'имя фамилия'
+ */
+var nameGenerate = function () {
+  var randomName = getRandomElementFromArray(WIZARD.NAMES);
+  var randomSurename = getRandomElementFromArray(WIZARD.SURNAMES);
+  var nameArray = [randomName, randomSurename];
+  return shuffleArray(nameArray).join(' ');
+};
+
+/**
+ * Генерирует массив моков, каждый элемент которого состоит из объекта
+ * @param  {number} wizardsCount - желаемое количевто элементов массива
+ * @return {Array} wizards - готовый массив с требуемой длиной
+ */
+var generateWizards = function (wizardsCount) {
+  var wizards = [];
+  for (var i = 0; i < wizardsCount; i++) {
+    wizards.push(
+        {
+          name: nameGenerate(),
+          coatColor: getRandomElementFromArray(WIZARD.COATS),
+          eyesColor: getRandomElementFromArray(WIZARD.EYES)
+        }
+    );
+  }
+  return wizards;
+};
+
+/**
+ * Создание DOM элемента на основе JS объекта
+ * @param {Object} wizard - элемент массива wizards
+ * @return {Object} wizardElement - клон ноды .setup-similar-item с рандомными данными
+ */
 var renderWizard = function (wizard) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
   return wizardElement;
 };
 
-// Заполнение блока DOM-элементами на основе массива JS-объектов
-var fragment = document.createDocumentFragment();
-wizards.forEach(function (item, index) {
-  fragment.appendChild(renderWizard(wizards[index]));
-});
-similarListElement.appendChild(fragment);
+/**
+ * Заполнение блока DOM-элементами на основе массива JS-объектов
+ * @param  {Array} wizards - массив, содержащий сгенерированные данные
+ */
+var pushWizards = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+  wizards.forEach(function (item) {
+    fragment.appendChild(renderWizard(item));
+  });
+  similarListElement.appendChild(fragment);
+};
 
+var userDialog = document.querySelector('.setup');
+var wizards = generateWizards(WIZARD.COUNT);
+userDialog.classList.remove('hidden');
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+pushWizards(wizards);
